33189c647c4111b62d36b9025025078d3fbdbc83
Fabian Mendoza <mendoza.fabian@hotmail.com>
2020-08-20 21:35:44 -0300

    Web App con Asp.Net

---
+++ UmbrellaCorpGestion_Mendoza/ClasesCustom/Encriptar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace UmbrellaCorpGestion_Mendoza.ClasesCustom
+{
+    public class Encriptar
+    {
+        public static string ConvertiraHash(string cadenaOriginal)
+        {
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+
+            byte[] inputBytes = (new UnicodeEncoding()).GetBytes(cadenaOriginal);
+            byte[] hash = sha1.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
---
+++ UmbrellaCorpGestion_Mendoza/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UmbrellaCorpGestion_Mendoza.Global" Language="C#" %>
